edca8a168631a1998c409d18072d996e799cdd0c
João Pedro Fabiano Franco <joao.franco@dextra-sw.com>
2018-08-02 20:53:12 -0300

    [FIX] check if exists type

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -22,39 +22,43 @@ function Navigation(controls) {
     }
 
     this.isFirst = function() {
-        return this.type.isFirst();
+        return this.type && this.type.isFirst();
     }
 
     this.isLast = function() {
-        return this.type.isLast();
+        return this.type && this.type.isLast();
     }
 
     this.focus = function() {
-        this.type.focus();
+        if (this.type) this.type.focus();
     }
         
     this.move = function(keycode) {
-        if (this.type.hasElements()) {
-            switch (keycode) {
-            case this.controls.left:
-                this.type.left();
-                break;
-            case this.controls.up:
-                this.type.up();
-                break;
-            case this.controls.down:
-                this.type.down();
-                break;
-            case this.controls.right:
-                this.type.right();
-                break;
-            case this.controls.return:
-            case 8:
-                this.onReturn();
-                break;
-            default: break;
+        if (type) {
+            if (this.type.hasElements()) {
+                switch (keycode) {
+                case this.controls.left:
+                    this.type.left();
+                    break;
+                case this.controls.up:
+                    this.type.up();
+                    break;
+                case this.controls.down:
+                    this.type.down();
+                    break;
+                case this.controls.right:
+                    this.type.right();
+                    break;
+                case this.controls.return:
+                case 8:
+                    this.onReturn();
+                    break;
+                default: break;
+                }
+                this.focus();
+            } else {
+                console.error('Please, set navigation type');
             }
-            this.focus();
         }
     }
 }
